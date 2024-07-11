84aebdc04407ce4d6a59a9636748d0cf5ad4de66
runtime-error786 <mustafa782a@gmail.com>
2024-07-12 01:01:39 +0500

    delete produuct ui added

--- food-delivery-frontend/src/app/Others/Protect.js
+++ food-delivery-frontend/src/app/Others/Protect.js
@@ -41,7 +41,7 @@ const Protect = ({ children }) => {
 
     if (role === "admin") {
         console.log("admin role")
-        if ( router === "/admin" || router === "/admin/addadmin" || router === "/admin/addproduct" ||  router === "/error" || router === "/admin/deladmin" ) {
+        if ( router === "/admin" || router === "/admin/addadmin" || router === "/admin/addproduct" ||  router === "/error" || router === "/admin/deladmin" || router === "/admin/delprod" ) {
             return <>{children}</>;
         } else {
             route.push("/error");
