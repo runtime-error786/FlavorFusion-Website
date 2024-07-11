7e07aab87a980a2f5408e4b657d5a945476fb8dd
runtime-error786 <mustafa782a@gmail.com>
2024-07-11 10:24:52 +0500

    sort feature added in del admin

--- server/signup/views.py
+++ server/signup/views.py
@@ -8,9 +8,13 @@ from rest_framework.decorators import action
 class CustomUserViewSet(viewsets.ModelViewSet):
     queryset = CustomUserDB.objects.all()
     serializer_class = CustomUserSerializer
-    
+
     @action(detail=False, methods=['get'], url_path='admins')
     def get_admins(self, request):
-        admins = CustomUserDB.objects.filter(role=CustomUserDB.ROLE_ADMIN)
+        sort = request.query_params.get('sort', 'true').lower()
+        if sort == 'true':
+            admins = CustomUserDB.objects.filter(role=CustomUserDB.ROLE_ADMIN).order_by('username')
+        else:
+            admins = CustomUserDB.objects.filter(role=CustomUserDB.ROLE_ADMIN).order_by('-username')
         serializer = self.get_serializer(admins, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
